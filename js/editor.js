0cb46e0921c6d58717a2fd9726047c6b9ea6cf2d
repaly <dimsemin2008@yandex.ru>
2018-02-12 22:29:47 +0300

    добавил отображение сегодняшней даты
    начинаю думать над заголовком, как правильно ресайзить

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,37 @@
+// задаю высоту редактору от высоты родителя, который отзывчив
 var pageHeight = document.querySelector('.page');
 pageHeight = window.getComputedStyle(pageHeight);
 pageHeight = pageHeight.getPropertyValue('height');
 
 document.querySelector('.main-editor').style.height = parseInt(pageHeight) - 80 + "px";
-// задаю высоту редактору от высоты родителя, который отзывчив
+
+// показываю текущую дату
+var now = new Date();
+var currentDay = now.getDate();
+var months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.',
+              'Sep.', 'Oct', 'Nov', 'Dec'];
+var currentMonth = months[now.getMonth()];
+var currentYear = now.getFullYear();
+var currentDate = currentMonth + " " + currentDay + " " + currentYear;
+
+document.querySelector('#current-time').append(currentDate);
+
+//
+var headerArticle = document.querySelector('#header-article');
+var counter = 25;
+
+
+function keyListener(){
+
+  console.log(headerArticle.value.length);
+
+  if (headerArticle.value.length >= counter) {
+    var headerFontSize = window.getComputedStyle(headerArticle).getPropertyValue('font-size');
+    headerArticle.style.fontSize = parseInt(headerFontSize) / 2 + "px";
+    counter += counter;
+    console.log("Counter = " + counter);
+  } else if (headerArticle.value.length < counter) {
+    
+  }
+
+}
